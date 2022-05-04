e4b8e70a4f924ee8d962faae5b52d6a1d20181db
Orcha02 <orcha2002@gmail.com>
2022-05-03 21:59:02 -0500

    Add Header to all routes

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,8 +3,8 @@ import "../styles/NotFound.css";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>Not Found</h1>
+    <div className="container">
+      <h1 className="error-title">Not Found</h1>
       <img
         className="error"
         src="https://raw.githubusercontent.com/Orcha02/holberton-system_engineering-devops/79f47bf24f1bd61898ea39eca89085bc5a442b2a/0x0C-web_server/404.jpg"
